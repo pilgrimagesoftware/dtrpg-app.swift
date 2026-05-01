dd36c7da82af399f4a738f3a2bdb08c7d86fcadb
Paul Schifferer <paul@schifferers.net>
2026-04-30 19:59:35 -0700

    feat: code

--- Sources/DTRPGClient/DTRPGClient.swift
+++ Sources/DTRPGClient/DTRPGClient.swift
@@ -1,16 +1,133 @@
 
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
 
-/**
-*/
-public class DTRPGClient {
+public struct AuthTokenResponse: Equatable, Sendable {
+    public let token: String
+    public let refreshToken: String
+    public let refreshTokenExpiresAt: Date
 
-    private let token : String
-
-/**
-*/
-    public init(token : String) {
+    public init(token: String, refreshToken: String, refreshTokenExpiresAt: Date) {
         self.token = token
+        self.refreshToken = refreshToken
+        self.refreshTokenExpiresAt = refreshTokenExpiresAt
+    }
+}
+
+public enum AuthState: String, Equatable, Sendable {
+    case unauthenticated
+    case tokenInvalid = "token_invalid"
+    case tokenExpired = "token_expired"
+    case refreshExpired = "refresh_expired"
+    case unauthorized
+}
+
+public struct AuthSessionError: Error, Equatable, Sendable {
+    public let errorCode: String
+    public let message: String
+    public let authState: AuthState
+
+    public init(errorCode: String, message: String, authState: AuthState) {
+        self.errorCode = errorCode
+        self.message = message
+        self.authState = authState
+    }
+}
+
+public struct AuthSession: Equatable, Sendable {
+    public let token: String
+    public let refreshToken: String
+    public let refreshTokenExpiresAt: Date
+
+    public init(token: String, refreshToken: String, refreshTokenExpiresAt: Date) {
+        self.token = token
+        self.refreshToken = refreshToken
+        self.refreshTokenExpiresAt = refreshTokenExpiresAt
+    }
+
+    public init(response: AuthTokenResponse) {
+        self.init(
+            token: response.token,
+            refreshToken: response.refreshToken,
+            refreshTokenExpiresAt: response.refreshTokenExpiresAt
+        )
+    }
+
+    public func isRefreshTokenExpired(at date: Date = Date()) -> Bool {
+        date >= refreshTokenExpiresAt
+    }
+}
+
+public enum SessionState: Equatable, Sendable {
+    case signedOut
+    case restoring
+    case authenticated(AuthSession)
+    case needsReauthentication(AuthSessionError?)
+}
+
+public struct SessionTransition: Equatable, Sendable {
+    public let previousState: SessionState
+    public let nextState: SessionState
+    public let error: AuthSessionError?
+
+    public init(previousState: SessionState, nextState: SessionState, error: AuthSessionError? = nil) {
+        self.previousState = previousState
+        self.nextState = nextState
+        self.error = error
+    }
+}
+
+public final class DTRPGClient {
+    public private(set) var sessionState: SessionState
+
+    public init(sessionState: SessionState = .signedOut) {
+        self.sessionState = sessionState
+    }
+
+    @discardableResult
+    public func beginRestoringSession() -> SessionTransition {
+        transition(to: .restoring)
+    }
+
+    @discardableResult
+    public func restoreSession(_ session: AuthSession, now: Date = Date()) -> SessionTransition {
+        if session.isRefreshTokenExpired(at: now) {
+            let error = AuthSessionError(
+                errorCode: AuthState.refreshExpired.rawValue,
+                message: "The stored refresh token has expired.",
+                authState: .refreshExpired
+            )
+            return transition(to: .needsReauthentication(error), error: error)
+        }
+
+        return transition(to: .authenticated(session))
+    }
+
+    @discardableResult
+    public func applyAuthResponse(_ response: AuthTokenResponse) -> SessionTransition {
+        transition(to: .authenticated(AuthSession(response: response)))
+    }
+
+    @discardableResult
+    public func invalidateSession(error: AuthSessionError) -> SessionTransition {
+        transition(to: .needsReauthentication(error), error: error)
+    }
+
+    @discardableResult
+    public func signOut() -> SessionTransition {
+        transition(to: .signedOut)
+    }
+
+    public var activeSession: AuthSession? {
+        guard case let .authenticated(session) = sessionState else {
+            return nil
+        }
+
+        return session
+    }
+
+    private func transition(to nextState: SessionState, error: AuthSessionError? = nil) -> SessionTransition {
+        let previousState = sessionState
+        sessionState = nextState
+        return SessionTransition(previousState: previousState, nextState: nextState, error: error)
     }
 }
--- Tests/DTRPGClientTests/DTRPGClientTests.swift
+++ Tests/DTRPGClientTests/DTRPGClientTests.swift
@@ -4,11 +4,104 @@ import XCTest
 
 final class DTRPGClientTests: XCTestCase {
 
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    func testClientStartsSignedOut() {
+        let client = DTRPGClient()
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        XCTAssertEqual(client.sessionState, .signedOut)
+        XCTAssertNil(client.activeSession)
+    }
+
+    func testApplyAuthResponseStoresAuthenticatedSession() {
+        let expiry = Date(timeIntervalSince1970: 1_800_000_000)
+        let response = AuthTokenResponse(
+            token: "access-token",
+            refreshToken: "refresh-token",
+            refreshTokenExpiresAt: expiry
+        )
+        let client = DTRPGClient()
+
+        let transition = client.applyAuthResponse(response)
+
+        let expectedSession = AuthSession(response: response)
+        XCTAssertEqual(transition.previousState, .signedOut)
+        XCTAssertEqual(transition.nextState, .authenticated(expectedSession))
+        XCTAssertEqual(client.sessionState, .authenticated(expectedSession))
+        XCTAssertEqual(client.activeSession, expectedSession)
+    }
+
+    func testRestoreSessionRejectsExpiredRefreshToken() {
+        let now = Date(timeIntervalSince1970: 1_800_000_000)
+        let expiredSession = AuthSession(
+            token: "access-token",
+            refreshToken: "refresh-token",
+            refreshTokenExpiresAt: now
+        )
+        let client = DTRPGClient(sessionState: .restoring)
+
+        let transition = client.restoreSession(expiredSession, now: now)
+
+        let expectedError = AuthSessionError(
+            errorCode: AuthState.refreshExpired.rawValue,
+            message: "The stored refresh token has expired.",
+            authState: .refreshExpired
+        )
+        XCTAssertEqual(transition.previousState, .restoring)
+        XCTAssertEqual(transition.nextState, .needsReauthentication(expectedError))
+        XCTAssertEqual(transition.error, expectedError)
+        XCTAssertEqual(client.sessionState, .needsReauthentication(expectedError))
+        XCTAssertNil(client.activeSession)
+    }
+
+    func testRestoreSessionAcceptsUnexpiredRefreshToken() {
+        let now = Date(timeIntervalSince1970: 1_800_000_000)
+        let session = AuthSession(
+            token: "access-token",
+            refreshToken: "refresh-token",
+            refreshTokenExpiresAt: now.addingTimeInterval(60)
+        )
+        let client = DTRPGClient(sessionState: .restoring)
+
+        let transition = client.restoreSession(session, now: now)
+
+        XCTAssertEqual(transition.previousState, .restoring)
+        XCTAssertEqual(transition.nextState, .authenticated(session))
+        XCTAssertEqual(client.activeSession, session)
+    }
+
+    func testInvalidateSessionMovesToReauthenticationState() {
+        let session = AuthSession(
+            token: "access-token",
+            refreshToken: "refresh-token",
+            refreshTokenExpiresAt: Date(timeIntervalSince1970: 1_800_000_000)
+        )
+        let error = AuthSessionError(
+            errorCode: AuthState.tokenExpired.rawValue,
+            message: "The access token has expired.",
+            authState: .tokenExpired
+        )
+        let client = DTRPGClient(sessionState: .authenticated(session))
+
+        let transition = client.invalidateSession(error: error)
+
+        XCTAssertEqual(transition.previousState, .authenticated(session))
+        XCTAssertEqual(transition.nextState, .needsReauthentication(error))
+        XCTAssertEqual(transition.error, error)
+        XCTAssertNil(client.activeSession)
+    }
+
+    func testSignOutClearsSessionState() {
+        let session = AuthSession(
+            token: "access-token",
+            refreshToken: "refresh-token",
+            refreshTokenExpiresAt: Date(timeIntervalSince1970: 1_800_000_000)
+        )
+        let client = DTRPGClient(sessionState: .authenticated(session))
+
+        let transition = client.signOut()
+
+        XCTAssertEqual(transition.previousState, .authenticated(session))
+        XCTAssertEqual(transition.nextState, .signedOut)
+        XCTAssertEqual(client.sessionState, .signedOut)
+        XCTAssertNil(client.activeSession)
     }
 }
